21b77678e0b80044176182dd13e595742b66b6b0
fran-cielle <55562858+fran-cielle@users.noreply.github.com>
2019-10-02 19:12:16 -0300

    23-09

---
+++ 23-09/exercicios.js
@@ -0,0 +1,37 @@
+//  Exercício 01
+
+/*   alert ("Olá Mundo!");  */
+
+
+//  Exercício 02
+
+/*
+  var n1, n2, n3
+  n1 = parseInt(prompt ("Digite um número"));
+  n2 = parseInt(prompt ("Digite outro número"));
+  n3 = alert (n1 + n2);
+*/
+
+//  Exercício 03
+
+/*
+var ano_nasc, idade, calc
+
+ano_nasc = parseInt(prompt("Digite seu ano de nascimento"));
+calc = 2019 - ano_nasc;
+idade = alert("Você tem " +calc+ " anos");
+*/
+
+//  Exercício 04
+
+
+/* 
+var n1,n2,n3,media
+
+ n1 = parseInt(prompt ("Digite o primeiro número"));
+ n2 = parseInt(prompt ("Digite o segundo número"));
+ n3 = parseInt(prompt ("Digite o terceiro número"));
+ media = (n1+n2+n3)/3;
+
+  alert("A média é "+media+" .");
+*/
